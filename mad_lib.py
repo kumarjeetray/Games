595e30484497bb5fda9aede8185c62f5e984f19c
Kumarjeet Ray <53223216+kumarjeetray@users.noreply.github.com>
2021-06-20 10:09:19 +0530

    Add files via upload

---
+++ mad_lib.py
@@ -0,0 +1,12 @@
+'''
+Roses are {color}
+{Plural noun} are blue
+I love {celebrity}
+'''
+
+color = input("Enter a color:")
+p_noun = input("Enter a plural noun:")
+celeb = input("Enter the name of a celebrity you like:")
+print("Roses are "+color)
+print(p_noun+" are blue")
+print("I love "+celeb)
